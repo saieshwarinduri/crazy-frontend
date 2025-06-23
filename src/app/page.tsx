f1622b20b01645cec2e5a087ea5b8eaf4e819a01
saieswar <saieswarindoori@gmail.com>
2025-06-23 22:42:32 +0530

    text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        Hello
         <Image
           className={styles.logo}
           src="/next.svg"
